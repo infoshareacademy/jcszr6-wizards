c884bed3518a76704b690d9ffd9afc3be7b075b2
Paweł Grajnert <pt.grajnert@gmail.com>
2022-05-26 15:32:28 +0200

    Implement methods in EquipmentMenagement

--- wizards/Wizards.BusinessLogic/Menagement/EquipmentMenagement.cs
+++ wizards/Wizards.BusinessLogic/Menagement/EquipmentMenagement.cs
@@ -1,26 +1,91 @@
 ﻿using System;
+using System.Linq;
+using Wizards.BusinessLogic.Model.Items;
 
 namespace Wizards.BusinessLogic
 {
     internal class EquipmentMenagement
     {
-        public void EquipItem(Hero hero, Item itemToEquip)
+        private Hero _hero;
+
+        public EquipmentMenagement(Hero hero)
         {
-            // TODO sprawdź czy item jest w inventorze gracza
+            _hero = hero;
+        }
+        public void EquipItem(Item itemToEquip)
+        {
+            if (_hero == null || itemToEquip == null)
+                return;
+
+            if (!_hero.Inventory.Contains(itemToEquip))
+                return;
+
             // TODO sprawdź czy gracz może założyć item
-            // TODO sprawdź czy gracz ma już założony item tego typu (np staff)
-            //      jeśli trzeba to podmień item jeśli nie to przenieś z inventory do equipped.
-            throw new NotImplementedException();
+
+            if (IsEquippedItemTypeOf(itemToEquip))
+            {
+                UnequipItem(GetEquippedItemTypeOf(itemToEquip));
+            }
+
+            if (_hero.Equipped.Count < 2)
+            {
+                _hero.Inventory.Remove(itemToEquip);
+                _hero.Equipped.Add(itemToEquip);
+            }
+
+            SortEquipment(_hero);
         }
 
-        public void UnequipItem(Hero hero, Item itemToUnequip)
+        private Item GetEquippedItemTypeOf(Item itemToEquip)
         {
-            throw new NotImplementedException();
+            if (itemToEquip is Weapon)
+            {
+                return _hero.Equipped.FirstOrDefault(i => i is Weapon);
+            }
+
+            if (itemToEquip is Armor)
+            {
+                return _hero.Equipped.FirstOrDefault(i => i is Armor);
+            }
+
+            return null;
+        }
+
+        private bool IsEquippedItemTypeOf(Item itemToEquip)
+        {
+            var isWeapon = itemToEquip is Weapon;
+            var hasEquippedWeapon = _hero.Equipped.Any(i => i is Weapon);
+
+            if (hasEquippedWeapon && isWeapon)
+            {
+                return true;
+            }
+
+            var isArmor = itemToEquip is Armor;
+            var hasEquippedArmor = _hero.Equipped.Any(i => i is Armor);
+            if (hasEquippedArmor && isArmor)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void UnequipItem(Item itemToUnequip)
+        {
+            if (_hero == null || itemToUnequip == null)
+                return;
+
+            if (_hero.Equipped.Any(i => i == itemToUnequip))
+            {
+                _hero.Inventory.Add(itemToUnequip);
+                _hero.Equipped.Remove(itemToUnequip);
+            }
         }
 
         public void SortEquipment(Hero hero)
         {
-            throw new NotImplementedException();
+            hero.Equipped = hero.Equipped.OrderBy(i => i.Name).ToList();
         }
 
     }
