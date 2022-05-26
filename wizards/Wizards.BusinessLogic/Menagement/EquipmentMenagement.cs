3063b8f36ff61457554b2ffd7ccf46db872f07c4
Paweł Grajnert <pt.grajnert@gmail.com>
2022-05-26 03:56:20 +0200

    Add EquipmentMenagement, ModelsMenagement with some methods

---
+++ wizards/Wizards.BusinessLogic/Menagement/EquipmentMenagement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Wizards.BusinessLogic
+{
+    internal class EquipmentMenagement
+    {
+        public void EquipItem(Hero hero, Item itemToEquip)
+        {
+            // TODO sprawdź czy item jest w inventorze gracza
+            // TODO sprawdź czy gracz może założyć item
+            // TODO sprawdź czy gracz ma już założony item tego typu (np staff)
+            //      jeśli trzeba to podmień item jeśli nie to przenieś z inventory do equipped.
+            throw new NotImplementedException();
+        }
+
+        public void UnequipItem(Hero hero, Item itemToUnequip)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SortEquipment(Hero hero)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ wizards/Wizards.BusinessLogic/Menagement/ModelsMenagement.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Wizards.BusinessLogic
+{
+    public class ModelsMenagement
+    {
+        public void AddPlayer(Player playerToAdd)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void AddHeroToPlayer(Hero heroToAdd, Player playerToAddFor)
+        {
+            playerToAddFor.Heroes.Add(heroToAdd);
+        }
+
+        public void AddItemToInventory(Item itemToAdd, Hero heroToAddFor)
+        {
+            heroToAddFor.Inventory.Add(itemToAdd);
+        }
+
+        public void ReplacePlayer(Player playerToReplace, Player newPlayer)
+        {
+            playerToReplace = newPlayer;
+        }
+
+        public void ReplaceHeroInPlayer(Hero heroToReplace, Hero newHero)
+        {
+            heroToReplace = newHero;
+        }
+
+        public void ReplaceItem(Item itemToReplace, Item newItem)
+        {
+            itemToReplace = newItem;
+        }
+
+        public void RemovePlayer(Player playerToRemove)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveHeroFromPlayer(Hero heroToRemove, Player playerToRemoveFrom)
+        {
+            playerToRemoveFrom.Heroes.Remove(heroToRemove);
+        }
+
+        public void RemoveItemFromInventory(Item itemToRemove, Hero heroToRemoveFrom)
+        {
+            heroToRemoveFrom.Inventory.Remove(itemToRemove);
+        }
+
+    }
+}
