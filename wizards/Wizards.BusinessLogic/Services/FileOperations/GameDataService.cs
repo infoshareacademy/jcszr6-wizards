c94204fb90e78c7da13fbd39186b1a57d3f08210
Paweł Grajnert <pt.grajnert@gmail.com>
2022-06-15 19:26:17 +0200

    Add IgameDataService with implementation in GameDataService

---
+++ wizards/Wizards.BusinessLogic/Services/FileOperations/GameDataService.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Wizards.BusinessLogic.Services.FileOperations
+{
+    public class GameDataService : IGameDataService
+    {
+        public void UpdateGameData()
+        {
+            var path = GetJsonDirectory();
+            var json = JsonConvert.SerializeObject(Repository.GetAllPlayers());
+            File.WriteAllText(path, json);
+        }
+
+        public void LoadGameData()
+        {
+            var path = GetJsonDirectory();
+            if (File.Exists(path))
+            {
+                var dataFile = File.ReadAllText(path);
+                var players = JsonConvert.DeserializeObject<List<Player>>(dataFile);
+                Repository.UpdateAllPlayers(players);
+            }
+        }
+
+        private static string GetJsonDirectory()
+        {
+            string result;
+            var directory = new DirectoryInfo(Environment.CurrentDirectory);
+
+            while (!directory.GetFiles("*.sln").Any())
+            {
+                directory = directory.Parent;
+            }
+            result = Path.Combine(directory.FullName, "Wizards.BusinessLogic", "Data", "data.json");
+            return result;
+        }
+    }
+}
