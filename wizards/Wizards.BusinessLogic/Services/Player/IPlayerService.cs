e40a70257828da8b674bcbcacb7c082e0c32e309
Paweł Grajnert <pt.grajnert@gmail.com>
2022-06-15 14:04:56 +0200

    Add IPlayerService and its implementation in PlayerService

---
+++ wizards/Wizards.BusinessLogic/Services/Player/IPlayerService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+
+namespace Wizards.BusinessLogic.Services
+{
+    public interface IPlayerService
+    {
+
+        void Add(Player player);
+        void DeleteById(int id);
+        void Update(int id, Player player);
+        IEnumerable<Player> GetAll();
+        Player GetById(int id);
+    }
+}
---
+++ wizards/Wizards.BusinessLogic/Services/Player/PlayerService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Wizards.BusinessLogic.Services
+{
+    public class PlayerService : IPlayerService
+    {
+        private List<Player> _players;
+
+        public PlayerService()
+        {
+            _players = Repository.GetAllPlayers();
+        }
+
+        public void Add(Player player)
+        {
+            //TODO: Validate player!
+            player.SetId(GetUniqueId());
+            _players.Add(player);
+            Repository.UpdateAllPlayers(_players);
+        }
+
+        public void DeleteById(int id)
+        {
+            var player = GetById(id);
+            _players.Remove(player);
+        }
+
+        public void Update(int id, Player player)
+        {
+            //TODO: Validate player!
+            var playerToUpdate = GetById(id);
+
+            playerToUpdate.Password = player.Password;
+            playerToUpdate.Email = player.Email;
+            playerToUpdate.DateOfBirth = player.DateOfBirth;
+        }
+
+        public IEnumerable<Player> GetAll()
+        {
+            return _players.AsEnumerable();
+        }
+
+        public Player GetById(int id)
+        {
+            var player = _players.FirstOrDefault(p => p.Id == id);
+
+            if (player != null)
+            {
+                return player;
+            }
+
+            throw new NullReferenceException($"There is no Player with id{id}!");
+        }
+
+        private int GetUniqueId()
+        {
+            var maxId = _players.Max(p => p.Id);
+            var uniqueId = 1;
+
+            if (maxId < _players.Count)
+            {
+                while (_players.Any(p => p.Id == uniqueId))
+                {
+                    uniqueId++;
+                }
+            }
+            else
+            {
+                uniqueId = maxId + 1;
+            }
+
+            return uniqueId;
+        }
+    }
+}
