f962faf3eeba4da2080cb931179f094114a081f1
Paweł Grajnert <pt.grajnert@gmail.com>
2022-06-22 22:20:33 +0200

    Add PasswordChange model for view

---
+++ wizards/Wizards.BusinessLogic/Model/ModelsForViews/PasswordChange.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Wizards.BusinessLogic
+{
+    public class PasswordChange
+    {
+        public Player Player { get; set; }
+        public string OldPassword { get; set; }
+
+        [Required]
+        [Display(Name ="Enter actual Password")]
+        [Compare("OldPassword", ErrorMessage = "Incorrect actual Password!")]
+        public string EnterOldPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Enter new Password")]
+        [MinLength(8)]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Confirm new Password")]
+        [MinLength(8)]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "New Password and Confirm new Password should be the same!")]
+        public string ConfirmPassword { get; set; }
+
+        public PasswordChange(Player player)
+        {
+            Player = player;
+        }
+
+        public Player GetPlayerWithNewPassword()
+        {
+            return new Player()
+            {
+                Id = Player.Id,
+                Password = this.NewPassword,
+                Email= Player.Email,
+                DateOfBirth= Player.DateOfBirth,
+            };
+        }
+    }
+}
