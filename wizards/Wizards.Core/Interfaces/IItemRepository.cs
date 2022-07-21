93eb83d02d475f28dd6f5420d9934cc25f60bdf1
JAKUB OCZKO <JAKUB.OCZKO@ing.com>
2022-07-21 18:35:16 +0200

    Add IItemRepository and implementation

---
+++ wizards/Wizards.Core/Interfaces/IItemRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wizards.Core.Model;
+
+namespace Wizards.Core.Interfaces
+{
+    public interface IItemRepository
+    {
+        Task<List<Item>> GetAll();
+        Task<Item> Get(int id);
+        Task Add(Item item);
+        Task Update(Item item);
+        Task Remove(Item item);
+    }
+}
---
+++ wizards/Wizards.Repository/Repository/ItemRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wizards.Core.Interfaces;
+using Wizards.Core.Model;
+
+namespace Wizards.Repository.Repository
+{
+    internal class ItemRepository : IItemRepository
+
+    {
+        private readonly WizardsContext _wizardsContext;
+
+        public ItemRepository(WizardsContext wizardsContext)
+        {
+            _wizardsContext = wizardsContext;
+        }
+
+        public async Task Add(Item item)
+        {
+            await _wizardsContext.AddAsync(item);
+            await _wizardsContext.SaveChangesAsync();
+        }
+
+        public async Task<Item> Get(int id)
+        {
+            return await _wizardsContext.Items.SingleOrDefaultAsync(i => i.Id == id);
+        }
+
+        public async Task<List<Item>> GetAll()
+        {
+            return await _wizardsContext.Items.ToListAsync();
+        }
+
+        public async Task Remove(Item item)
+        {
+            _wizardsContext.Remove(item);
+            await _wizardsContext.SaveChangesAsync();
+        }
+
+        public async Task Update(Item item)
+        {
+            _wizardsContext.Update(item);
+            await _wizardsContext.SaveChangesAsync();
+        }
+    }
+}
