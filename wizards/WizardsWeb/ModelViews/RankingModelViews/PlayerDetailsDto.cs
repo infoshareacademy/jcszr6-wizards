de019c5542d63c8754f9fcf457e175383d615759
Paweł Grajnert <pt.grajnert@gmail.com>
2022-08-19 00:08:27 +0200

    corrections after merge;

--- wizards/WizardsWeb/ModelViews/RankingModelViews/PlayerDetailsDto.cs
+++ wizards/WizardsWeb/ModelViews/RankingModelViews/PlayerDetailsDto.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using Microsoft.VisualBasic;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace WizardsWeb.ModelViews.RankingModelViews;
-    public class PlayerDetailsDto
-    {
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public int HeroNumber { get; set; }
-        public int RankNumber { get; set; }
-        public int GoldHeroNumber { get; set; }
-
+public class PlayerDetailsDto
+{
+    [Display(Name ="User Name")]
+    public string UserName { get; set; }
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+    [Display(Name = "Hero's Count")]
+    public int HeroNumber { get; set; }
+    [Display(Name = "Rank Points")]
+    public int RankNumber { get; set; }
+    [Display(Name = "Total Gold")]
+    public int GoldHeroNumber { get; set; }
 }
-
