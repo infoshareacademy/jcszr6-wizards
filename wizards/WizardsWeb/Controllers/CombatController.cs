43af756d3ee82dcbb7a549197a31d619f9eaaa49
Paweł Grajnert <pt.grajnert@gmail.com>
2022-09-08 19:06:35 +0200

    Add partial views; Add exploration model views and exploration controller;

--- wizards/WizardsWeb/Controllers/CombatController.cs
+++ wizards/WizardsWeb/Controllers/CombatController.cs
@@ -8,13 +8,13 @@ using WizardsWeb.ModelViews.CombatModelViews;
 
 namespace WizardsWeb.Controllers
 {
-    public class BattleStageController : Controller
+    public class CombatController : Controller
     {
         private readonly IStageService _stageService;
         private readonly ICombatStageInstancesRepository _stageRepository;
         private readonly IMapper _mapper;
 
-        public BattleStageController(IStageService stageService, ICombatStageInstancesRepository stageRepository, IMapper mapper)
+        public CombatController(IStageService stageService, ICombatStageInstancesRepository stageRepository, IMapper mapper)
         {
             _stageService = stageService;
             _stageRepository = stageRepository;
@@ -24,26 +24,34 @@ namespace WizardsWeb.Controllers
         public async Task<IActionResult> Index()
         {
             var combatStage = await _stageRepository.GetAsync(User.GetId());
-            var battleStage = _mapper.Map<CombatStageModelView>(combatStage); 
+            
+            var battleStage = _mapper.Map<CombatStageModelView>(combatStage);
+            
+            combatStage.LastRoundResult = null;
 
-            return View("BattleStage", battleStage);
+            return View("CombatStage", battleStage);
         }
 
-        public IActionResult CreateNewMatch(int enemyId)
+        public async Task<IActionResult> CreateNewMatch(int enemyId)
         {
-            
+            await _stageService.CreateNewMatchAsync(User.GetId(), enemyId);
+
             return RedirectToAction("Index");
         }
 
         [HttpPost]
-        public IActionResult CommitRound(CombatStageModelView battleStage)
+        public async Task<IActionResult> CommitRound(CombatStageModelView battleStage)
         {
+            var nextHeroSkillId = battleStage.HeroSelectedSkillId;
+
+            await _stageService.CommitRoundAsync(User.GetId(), nextHeroSkillId);
 
             return RedirectToAction("Index");
         }
 
-        public IActionResult FinishMatch()
+        public async Task<IActionResult> FinishMatch()
         {
+            await _stageService.FinishMatchAsync(User.GetId());
 
             return RedirectToAction("Details", "Hero");
         }
---
+++ wizards/WizardsWeb/Mapping/ExplorationProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Wizards.Core.Model.WorldModels;
+using WizardsWeb.ModelViews.ExplorationModelViews;
+
+namespace WizardsWeb.Mapping;
+
+public class ExplorationProfile : Profile
+{
+    public ExplorationProfile()
+    {
+        CreateMap<Enemy, EnemyIndexModelView>();
+    }
+}
