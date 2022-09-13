70d41458d2240d17bc3625bf3cfcd85558431d03
Paweł <55764496+pdawic@users.noreply.github.com>
2022-09-13 13:37:36 +0200

    add exceptions to check

--- wizards/WizardsWeb/Startup.cs
+++ wizards/WizardsWeb/Startup.cs
@@ -78,10 +78,23 @@ public class Startup
         }
         else
         {
-            app.UseExceptionHandler("/Home/Error");
+            // app.UseExceptionHandler("/Home/Error");
+            
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
+
+        app.UseMiddleware<MyExceptionHandler>();
+
+        app.Use(async (context, next) =>
+        {
+            await next();
+            if (context.Response.StatusCode == StatusCodes.Status404NotFound)
+            {
+                context.Request.Path = "/Home/Error404";
+                await next();
+            }
+        });
         
         app.UseHttpsRedirection();
         app.UseStaticFiles();
