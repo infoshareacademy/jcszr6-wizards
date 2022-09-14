59886226780f94a46c6fe8586d1fc5ebf1b8b111
Paweł <55764496+pdawic@users.noreply.github.com>
2022-09-14 22:40:30 +0200

    add first test

---
+++ wizards/Wizards.Tests/CoreTests/EnemyExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wizards.Core.Model.WorldModels;
+using Wizards.Core.Model.WorldModels.Properties;
+using Wizards.Core.ModelExtensions;
+
+namespace Wizards.Tests.CoreTests
+{
+    public class EnemyExtensionsTests
+    {
+        [Fact]
+        public void CalculateSkillDamage_WithNullAsEnemy_ThrowException()
+        {
+            var enemySkill = new EnemySkill();
+            Enemy enemy = null;
+            var result = () => enemy.CalculateSkillDamage(enemySkill);
+
+            result.Should().Throw<ArgumentNullException>();
+
+        }
+
+    }
+}
