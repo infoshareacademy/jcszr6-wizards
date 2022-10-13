592aac31aed741ec4f64e7121426a5212a64c91b
JAKUB OCZKO <JAKUB.OCZKO@ing.com>
2022-10-13 22:30:42 +0200

    LogSender and ILogSender added

---
+++ wizards/Wizards.LogsSender/LogRecord.cs
@@ -0,0 +1,12 @@
+﻿namespace Wizards.LogsSender
+{
+    public class LogRecord
+    {
+        public DateTime TimeStamp { get; set; }
+        public string AppName { get; set; }
+        public string LogLevel { get; set; }
+        public string LogMessage { get; set; }
+        public string Exception { get; set; }
+        public string Properties { get; set; }
+    }
+}
